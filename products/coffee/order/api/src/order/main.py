3a468772526acc53288ddef6bb358bff1074006d
Arine <peppydays@gmail.com>
2023-08-13 22:57:24 +0900

    Modified REST API URL from order to orders

--- products/coffee/order/api/src/order/main.py
+++ products/coffee/order/api/src/order/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from order.interface import router as cart_router
 
 app = FastAPI()
-app.include_router(router=cart_router, prefix="/order")
+app.include_router(router=cart_router, prefix="/orders")
 
 
 @app.get("/")
